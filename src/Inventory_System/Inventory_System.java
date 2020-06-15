8a77d4a523b973ea6ebfb3ea628af5e3bf140778
rider7 <brandon.rider@gmail.com>
2020-06-14 21:57:17 -0500

    GUI added

--- src/Inventory_System/Inventory_System.java
+++ src/Inventory_System/Inventory_System.java
@@ -1,9 +1,25 @@
 package Inventory_System;
 
-public class Inventory_System {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Inventory_System extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View_Controller/Main_Screen.fxml"));
+        primaryStage.setTitle("Brandon Rider Inventory System Application");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
 
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
